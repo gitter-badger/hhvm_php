9ed536d5bc99fa60611ee7f2a1cac9bdf4c32a16
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2015-01-13 20:34:29 +0200

    Check PHP versions

---
+++ checker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in $(find ./scripts/ -name *.php -type f); do
+  echo $i;
+  echo -ne "\tPHP  - "; php -f $i; if [ $? -eq 0 ]; echo "Ok"; else echo "Fail"; fi;
+  echo -ne "\tHHVM - "; hhvm -f $i; if [ $? -eq 0 ]; echo "Ok"; else echo "Fail"; fi;
+done;
